28205f6cfad47d13c19fb6779116d96740338cf5
root <root@localhost.localdomain>
2015-04-11 20:59:19 +0530

    Import function program

---
+++ hd.py
@@ -0,0 +1,11 @@
+def hour():
+	h = int(input("ENTER HOUR::"))
+	return h
+def day():
+	d = int(input("ENTER DAY::"))
+	return d
+h = hour()
+d = day()
+def cost(h,d):
+	l = h*d*8
+	return l
---
+++ koimport.py
@@ -0,0 +1,8 @@
+import hd
+def main():
+	m = hd.h
+	l = hd.d
+	k = hd.cost(m,l)
+	print 'cost is the::',k
+if __name__ == '__main__':
+	main()
